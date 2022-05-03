8182803331a409dbd7f22086a320bfac40cb024f
Harshal <ranjhaniharshal@gmail.com>
2022-05-03 22:36:29 +0530

    add marvel.ejs

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/quiz/hunger-games", (req, res) => {
   res.send(`<h1>HUNGER GAMES</h1>`);
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening on port {port}`);
+  console.log(`Listening on port ${port}`);
 });
